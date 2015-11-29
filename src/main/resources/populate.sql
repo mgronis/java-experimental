031d998e938b3c7063efc7571acd7d45302da241
mgronis <m.gronis@gmail.com>
2015-11-29 12:48:39 +0100

    Added script for populating db table with test data

---
+++ src/main/resources/populate.sql
@@ -0,0 +1,8 @@
+USE javaexperimental;
+
+INSERT INTO testing VALUES ('Gunnar', 15, FALSE);
+INSERT INTO testing VALUES ('Lisa', 18, TRUE);
+INSERT INTO testing VALUES ('Sverker', 65, FALSE);
+INSERT INTO testing VALUES ('Anna', 12, TRUE);
+INSERT INTO testing VALUES ('Julia', 25, TRUE);
+INSERT INTO testing VALUES ('Pelle', 30, TRUE );
