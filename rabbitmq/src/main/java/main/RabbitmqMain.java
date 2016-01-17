29f8abbd072e74daeb6926aa92e03c99fc67293b
mgronis <m.gronis@gmail.com>
2016-01-17 18:06:05 +0100

    Added a first test of RabbitMQ

--- rabbitmq/src/main/java/main/RabbitmqMain.java
+++ rabbitmq/src/main/java/main/RabbitmqMain.java
@@ -1,9 +1,34 @@
 package main;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class RabbitmqMain {
 
-    public static void main(String[] args) {
+    public static final String QUEUE = "somequeue";
 
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE, false, false, false, null);
+        channel.basicPublish("", QUEUE, null, "hello there!".getBytes());
+
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println(new String(body, "UTF-8"));
+            }
+        };
+        channel.basicConsume(QUEUE, true, consumer);
+
+        channel.close();
+        connection.close();
     }
 
 }
