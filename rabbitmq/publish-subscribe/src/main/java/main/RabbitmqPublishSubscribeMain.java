3be04b9104a9a5b68ca5db295548186541665a01
mgronis <m.gronis@gmail.com>
2016-02-07 18:16:36 +0100

    Added Publish/Subscriber test project

---
+++ rabbitmq/publish-subscribe/src/main/java/main/RabbitmqPublishSubscribeMain.java
@@ -0,0 +1,49 @@
+package main;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitmqPublishSubscribeMain {
+
+    public static final String EXCHANGE = "some_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE, "fanout");
+
+        String queue1 = channel.queueDeclare().getQueue();
+        String queue2 = channel.queueDeclare().getQueue();
+
+        channel.queueBind(queue1, EXCHANGE, "");
+        channel.queueBind(queue2, EXCHANGE, "");
+
+        Consumer consumer1 = createConsumer(channel, "Elin");
+        Consumer consumer2 = createConsumer(channel, "Malin");
+
+        channel.basicConsume(queue1, true, consumer1);
+        channel.basicConsume(queue2, true, consumer2);
+
+        channel.basicPublish(EXCHANGE, "", null, "Hello there!".getBytes());
+
+        Thread.sleep(500L);
+
+        channel.close();
+        connection.close();
+    }
+
+    private static DefaultConsumer createConsumer(final Channel channel, String name) {
+        return new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println(name + ": " + new String(body, "UTF-8"));
+            }
+        };
+    }
+
+}
